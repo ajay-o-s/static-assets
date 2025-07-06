979dc6a0fe97a8ab7728b85b6a1042cbe73db99a
Ajay o s <ajayosakhub@gmail.com>
2025-07-06 12:55:44 +0530

    🎨 Add global static assets for docs (styles.css, script.js)

--- js/docs/script.js
+++ js/docs/script.js
@@ -1,4 +1,26 @@
 function toggleTheme() {
-  document.body.classList.toggle('dark');
-  document.body.classList.toggle('light');
+  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+
+  const themeIcon = document.getElementById("theme-icon");
+  if (document.body.classList.contains("dark")) {
+    themeIcon.textContent = "☀️"; // Sun icon
+  } else {
+    themeIcon.textContent = "🌙"; // Moon icon
+  }
 }
+
+// Set year on load
+const currentYear = new Date().getFullYear();
+document.getElementById("footer-year").textContent = currentYear;
+document.getElementById("last-updated-year").textContent = currentYear;
+
+// Set default icon on load
+window.addEventListener("DOMContentLoaded", () => {
+  const themeIcon = document.getElementById("theme-icon");
+  if (document.body.classList.contains("dark")) {
+    themeIcon.textContent = "☀️";
+  } else {
+    themeIcon.textContent = "🌙";
+  }
+});
